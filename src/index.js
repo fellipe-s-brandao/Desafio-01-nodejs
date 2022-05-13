3881ceaa047a7bc87b38149ba0c3e7d1abf8a471
fellipebrandao <fellipebrandao@deltaglobal.com.br>
2022-05-12 22:51:37 -0300

    inicion desafio

--- src/index.js
+++ src/index.js
@@ -1,29 +1,81 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+let users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if(user.length == 0) {
+    return response.status(404).json({ error: 'Usuário não encontrado'});
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  if(name && username) {
+    const userAlreadyExists = users.some(
+      (user) => user.name === name
+    );
+
+    if(userAlreadyExists) {
+      return response.status(400).json({ error: 'Já existe um usuário criado com este nome!'})
+    }
+
+    const user = {
+      id: uuidv4(),
+      name,
+      username,
+      todos: []
+    };
+    
+    users.push(user)
+
+    return response.status(201).json(user);
+
+  } else {
+    response.status(400).json({ error: 'Preencha todos os dados!'})
+  }
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.todos.length > 0) {
+    response.status(200).json(user.todos)
+  } else {
+    response.status(401).json({ error: 'Usuário não possui tarefas!'})
+  }
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(), 
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
